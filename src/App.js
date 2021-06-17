f4fa0c8fa4b63afbe8d82c7b6562e4eabd1efcf1
HarshitChauhan <harshitkumar2503@gmail.com>
2021-06-17 23:55:47 +0530

    Routers and uuid setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import TextEditor from "./TextEditor";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 function App() {
   return (
-    <TextEditor/>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidv4()}`} />
+        </Route>
+        <Route path="/documents/:id" exact>
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
